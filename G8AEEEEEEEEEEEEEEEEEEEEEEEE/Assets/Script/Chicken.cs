c41b02c96e65e9f2f1a921ba762b30c4eb7308f9
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2019-10-30 15:58:16 +0800

    G8雞開始程式

---
+++ G8AEEEEEEEEEEEEEEEEEEEEEEEE/Assets/Script/Chicken.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+
+    Rigidbody ridi;
+    public int speed;
+
+    private void Start()
+    {
+        ridi = GetComponent<Rigidbody>();
+    }
+
+    public static void Updata() {
+
+
+
+
+    }
+
+|}
