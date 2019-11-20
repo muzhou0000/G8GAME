061b3887f3291d40cf3b61676dc080c4bc69c793
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2019-11-20 15:53:11 +0800

    程式-方法

--- G8AEEEEEEEEEEEEEEEEEEEEEEEE/Assets/Script/Chicken.cs
+++ G8AEEEEEEEEEEEEEEEEEEEEEEEE/Assets/Script/Chicken.cs
@@ -4,18 +4,57 @@ public class Chicken : MonoBehaviour
 {
 
     Rigidbody ridi;
+    [Header("移動速度"), Range(1, 100), Tooltip("G8雞的")]
     public int speed;
+
+    void run(int speed)
+    {
+        print("G8雞的跑步速度："+speed);
+    }
+    void turn(float turning)
+    {
+        print("G8雞的旋轉：" + turning);
+
+    }
+    void yell(string yell="GGGGGGGGGGGGGGGGGGGGGGGGGG")
+    {
+        print("G8雞的叫聲：" + yell);
+
+    }
+    void take(string say="幹")
+    {
+        print("G8雞說：" + say);
+
+    }
+    void viewmission(string see="正在檢視")
+    {
+        print("G8雞" + see);
+
+    }
+
+    void G8chicken_motion(int speed, float turning, string yell = "88888888888888888888", string say = "幹!", string see = "正在檢視...")
+    {
+        print("G8雞的跑步速度：" + speed);
+        print("G8雞的旋轉：" + turning);
+        print("G8雞的叫聲：" + yell);
+        print("G8雞說：" + say);
+        print("G8雞" + see);
+
+    }
 
     private void Start()
     {
         ridi = GetComponent<Rigidbody>();
-    }
-
-    public static void Updata() {
-
-
-
+        G8chicken_motion(10, 8);
 
     }
 
-|}
+    public static void Updata()
+    {
+
+
+
+
+    }
+
+}
---
+++ G8AEEEEEEEEEEEEEEEEEEEEEEEE/Assets/Script/Learn.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class Learn : MonoBehaviour
+{
+
+    private void Drive(int speed)
+    {
+        print("車子噗噗起洽起洽"+speed);
+    }
+
+    private void Love (int times,string name="Tony",string zzcoco="123")
+    {
+        print("對著" + name + "說");
+        print("I LOVE YOU " + times);
+        print("zzcoco" + zzcoco);
+    }
+
+    private void Start()
+    {
+        print("HELL-o");
+
+        Drive(200);
+        Drive(400);
+
+        Love(1000);
+        Love(1400,"456");
+        Love(1800,zzcoco:"456");
+        Love(2000,"marry");
+        Love(3000);
+        
+
+
+    }
+}
