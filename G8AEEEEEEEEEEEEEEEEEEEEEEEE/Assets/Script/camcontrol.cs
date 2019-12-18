3f131907c5b8546fd2d4e773b7b79d3118fe99af
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2019-12-18 16:03:47 +0800

    放食物

---
+++ G8AEEEEEEEEEEEEEEEEEEEEEEEE/Assets/Script/camcontrol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camcontrol : MonoBehaviour
+{
+    public Transform cam, chicken;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Vector3.Lerp(cam.position, chicken.position,0.5f * Time.deltaTime);
+        cam.position = pos;
+    }
+}
